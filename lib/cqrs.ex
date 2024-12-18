288d68d3bae6ad0f3580c62b5cfd8c303bfc0477
Pascal Charbonneau <pcharbonneau@lecoindustries.com>
2024-12-18 07:13:49 -0500

    Added mix project

---
+++ lib/cqrs.ex
@@ -0,0 +1,18 @@
+defmodule Cqrs do
+  @moduledoc """
+  Documentation for `Cqrs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cqrs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
